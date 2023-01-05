8188c0d363c8ca96e209f624015b269acc94de31
Ajay Krishna K V <55079486+AJAYK-01@users.noreply.github.com>
2023-01-05 08:23:46 +0530

    bug signin

--- pages/signin.js
+++ pages/signin.js
@@ -4,67 +4,70 @@ import Router from 'next/router';
 import axios from 'axios';
 
 export default function SignIn({ rows }) {
-  const [formData, setFormData] = useState({});
-  const [error, setError] = useState('');
+    const [formData, setFormData] = useState({});
+    const [error, setError] = useState('');
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const ph = formData['phone'];
-      const pwd = formData['password'];
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        var flag = 0;
+        try {
+            const ph = formData['phone'];
+            const pwd = formData['password'];
 
-      rows.forEach((row) => {
-        if (row.phone === ph) {
-          if (pwd === row.password) {
-            alert('Sign in success');
-            Router.push('/addcycle');
-          }
+            rows.forEach((row) => {
+                if (row.phone === ph) {
+                    if (pwd === row.password) {
+                        flag = 1;
+                        alert('Sign in success');
+                        Router.push('/addcycle');
+                    }
+                }
+            });
+            if (flag === 0) {
+                alert('Wrong phone or password, try again!');
+            }
+        } catch (error) {
+            setError(error.message);
         }
-      });
+    };
 
-      alert('Wrong phone or password, try again!');
-    } catch (error) {
-      setError(error.message);
-    }
-  };
-
-  return (
-    <div style={{ textAlign: 'center' }}>
-      <h1 style={{ fontSize: '24px', marginBottom: '20px' }}>Sign In</h1>
-      <form onSubmit={handleSubmit} style={{ display: 'inline-block' }}>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-        <label htmlFor="phone" style={{ display: 'block', marginBottom: '10px' }}>
-          Phone:
-          <input
-            type="text"
-            name="phone"
-            value={formData.phone}
-            onChange={handleChange}
-            style={{ width: '100%', marginTop: '10px' }}
-          />
-        </label>
-        <br />
-        <label htmlFor="password" style={{ display: 'block', marginBottom: '10px' }}>
-          Password:
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            style={{ width: '100%', marginTop: '10px' }}
-          />
-        </label>
-        <br />
-        <button type="submit" style={{ marginTop: '20px', padding: '10px 20px', fontSize: '16px' }}>
-          Sign In
-        </button>
-      </form>
-    </div>
-  );
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h1 style={{ fontSize: '24px', marginBottom: '20px' }}>Sign In</h1>
+            <form onSubmit={handleSubmit} style={{ display: 'inline-block' }}>
+                {error && <p style={{ color: 'red' }}>{error}</p>}
+                <label htmlFor="phone" style={{ display: 'block', marginBottom: '10px' }}>
+                    Phone:
+                    <input
+                        type="text"
+                        name="phone"
+                        value={formData.phone}
+                        onChange={handleChange}
+                        style={{ width: '100%', marginTop: '10px' }}
+                    />
+                </label>
+                <br />
+                <label htmlFor="password" style={{ display: 'block', marginBottom: '10px' }}>
+                    Password:
+                    <input
+                        type="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                        style={{ width: '100%', marginTop: '10px' }}
+                    />
+                </label>
+                <br />
+                <button type="submit" style={{ marginTop: '20px', padding: '10px 20px', fontSize: '16px' }}>
+                    Sign In
+                </button>
+            </form>
+        </div>
+    );
 }
 
 export const getServerSideProps = async (context) => {
