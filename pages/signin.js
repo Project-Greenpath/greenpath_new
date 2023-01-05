e18835fbd9386201440da21b9a03e2aed19cb7d8
GameGodS3 <sudevssuresh@gmail.com>
2023-01-05 12:28:59 +0530

    Added: sign in css

--- pages/signin.js
+++ pages/signin.js
@@ -2,6 +2,7 @@ import styles from "../styles/Home.module.css";
 import { useState } from "react";
 import Router from "next/router";
 import axios from "axios";
+import Link from "next/link";
 
 export default function SignIn({ rows }) {
   const [formData, setFormData] = useState({});
@@ -36,45 +37,90 @@ export default function SignIn({ rows }) {
   };
 
   return (
-    <div>
-      <h1 style={{ fontSize: "24px", marginBottom: "20px" }}>Sign In</h1>
-      <form onSubmit={handleSubmit} style={{ display: "inline-block" }}>
-        {error && <p style={{ color: "red" }}>{error}</p>}
-        <label
-          htmlFor="phone"
-          style={{ display: "block", marginBottom: "10px" }}
-        >
-          Phone:
-          <input
-            type="text"
-            name="phone"
-            value={formData.phone}
-            onChange={handleChange}
-            style={{ width: "100%", marginTop: "10px" }}
-          />
-        </label>
-        <br />
-        <label
-          htmlFor="password"
-          style={{ display: "block", marginBottom: "10px" }}
-        >
-          Password:
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            style={{ width: "100%", marginTop: "10px" }}
-          />
-        </label>
-        <br />
-        <button
-          type="submit"
-          style={{ marginTop: "20px", padding: "10px 20px", fontSize: "16px" }}
-        >
-          Sign In
-        </button>
-      </form>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <nav>
+        <div className="logo">
+          <Link href="/">
+            <img
+              src="/images/greenpath-logo-long.svg"
+              alt=""
+              className="logo-img"
+            />
+          </Link>
+        </div>
+        <div className="nav-menu">
+          <ul className="nav-links">
+            <Link href="/">
+              <li className="nav-item">Home</li>
+            </Link>
+            <Link href="/signup">
+              <li className="nav-item cta-secondary">Sign Up</li>
+            </Link>
+            <Link href="/signin">
+              <li className="nav-item cta-sec">Log In</li>
+            </Link>
+          </ul>
+        </div>
+      </nav>
+      <h1 style={{ fontSize: "24px" }}>Sign In</h1>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100%",
+          height: "100vh",
+        }}
+      >
+        <form onSubmit={handleSubmit}>
+          {error && <p style={{ color: "red" }}>{error}</p>}
+          <label
+            htmlFor="phone"
+            style={{ display: "block", marginBottom: "10px" }}
+          >
+            Phone:
+            <input
+              type="text"
+              name="phone"
+              value={formData.phone}
+              onChange={handleChange}
+              style={{ width: "100%", marginTop: "10px" }}
+            />
+          </label>
+          <br />
+          <label
+            htmlFor="password"
+            style={{ display: "block", marginBottom: "10px" }}
+          >
+            Password:
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              style={{ width: "100%", marginTop: "10px" }}
+            />
+          </label>
+          <br />
+          <button
+            type="submit"
+            style={{
+              marginTop: "20px",
+              padding: "10px 20px",
+              fontSize: "16px",
+            }}
+          >
+            Sign In
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
